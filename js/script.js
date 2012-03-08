8bf1cd582debc4a91d1e1361acc8f87cdbc19f99
Josh Ellington <hello@joshellington.com>
2012-03-07 22:16:07 -0800

    testing pulling blog posts

--- js/script.js
+++ js/script.js
@@ -4,5 +4,24 @@
 
 
 
+$(function() {
+  getBlog('sarahmichelle');
+});
 
 
+function getBlog(id) {
+  $.getJSON('/inc/tumblr.php/?id='+id, function(d) {
+    log(d);
+    parseBlog(d);
+  });
+}
+
+function parseBlog(d) {
+  var cont = $('#blog');
+
+  $.each(d.response.posts, function(i,item) {
+    if ( item.title ) {
+      cont.append('<div class="blog-post"><h2>'+item.title+'</h2></div>');
+    }
+  });
+}
